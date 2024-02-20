6e9244b3b9ccd6dd2ed7c5ffa97017b8698bf4a2
Azis Ramdhan <azis.darojat@gli.id>
2024-02-20 15:13:30 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by gli-macbook-azis on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
